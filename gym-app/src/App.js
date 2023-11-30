d44d86b34d617e222d09fd59cf62345e99549d75
Randy T <randythai360@gmail.com>
2023-11-30 16:57:49 -0600

    testing files

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import sidebar from './components/sidebar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Gym App is cool
         </p>
+
+        <sidebar />
+
       </header>
     </div>
   );
---
+++ gym-app/src/components/sidebar.js
@@ -0,0 +1,15 @@
+
+
+function sidebar() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          this is a sidebar
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default sidebar;
