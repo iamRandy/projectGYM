94dd5162cdd045e6cba1292209ffbd7e6e539c44
Aaron Luu <aaronluu@infra04-wg012.eduroam.wireless.umn.edu>
2023-12-07 16:45:33 -0600

    Changed login

--- gym-app/src/App.js
+++ gym-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Gym App
+          Login
         </p>
       </header>
     </div>
